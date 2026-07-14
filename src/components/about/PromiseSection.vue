5ce75a285b9fccaf4acff37878a194d62273d8be
bigdmike <piggy5272@gmail.com>
2026-07-14 20:54:21 +0800

    更新圖片路徑以符合靜態頁面使用

--- src/components/about/PromiseSection.vue
+++ src/components/about/PromiseSection.vue
@@ -3,43 +3,43 @@
 const promiseList = [
     {
         title:"水電配線指定品牌—太平洋電線（或同等級品牌）",
-        image:"/img/about/promise-1.png",
+        image:"/rm-design/img/about/promise-1.png",
     },
     {
         title:"水電配線指定年份—3年內",
-        image:"/img/about/promise-2.png",
+        image:"/rm-design/img/about/promise-2.png",
     },
     {
         title:"插座電線配線指定線徑 2.0mm",
-        image:"/img/about/promise-3.png",
+        image:"/rm-design/img/about/promise-3.png",
     },
     {
         title:"專用插座電線迴路指定截面積5.5mm",
-        image:"/img/about/promise-4.png",
+        image:"/rm-design/img/about/promise-4.png",
     },
     {
         title:"全室地板防水層",
-        image:"/img/about/promise-5.png",
+        image:"/rm-design/img/about/promise-5.png",
     },
     {
         title:"浴廁空間全室五道防水層",
-        image:"/img/about/promise-6.png",
+        image:"/rm-design/img/about/promise-6.png",
     },
     {
         title:"浴廁空間試水 72 小時",
-        image:"/img/about/promise-7.png",
+        image:"/rm-design/img/about/promise-7.png",
     },
     {
         title:"磁磚雙面塗抹黏著劑",
-        image:"/img/about/promise-8.png",
+        image:"/rm-design/img/about/promise-8.png",
     },
     {
         title:"設置沉澱桶",
-        image:"/img/about/promise-9.png",
+        image:"/rm-design/img/about/promise-9.png",
     },
     {
         title:"鋁窗填灌水泥、防水處理",
-        image:"/img/about/promise-10.png",
+        image:"/rm-design/img/about/promise-10.png",
     }
 ]
 
